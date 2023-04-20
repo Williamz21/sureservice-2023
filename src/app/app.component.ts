d85b84da4d49204169753e629f508d046c0b7ea3
Williamz21 <u201816224@upc.edu.pe>
2023-04-20 00:37:08 -0500

    responsive updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +9,9 @@ import { Router } from '@angular/router';
 export class AppComponent implements OnInit{
   title = "sureservice";
   classToggled=false;
+  topBarDisplay=false;
+  sideNav=document.getElementById('sideNav') as any;
+  open=true;
 
   constructor(public router: Router){
     
@@ -16,11 +19,55 @@ export class AppComponent implements OnInit{
   
   ngOnInit(): void {
     document.body.classList.add("bodyOnAll");
+    this.onWindowResize()
   }
 
   toggleBar(){
     this.classToggled=!this.classToggled;
+    this.open=!this.open;
     document.body.classList.toggle('bodyWidth');
     document.body.classList.toggle("bodyOnAll");
+  }
+
+  toggleBarMobile(){
+    this.topBarDisplay=!this.topBarDisplay;
+    document.body.classList.toggle('bodyMobile');
+    document.body.classList.toggle('bodyWidth');
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    if(window.innerWidth<1200 && window.innerWidth>760){
+      if(document.body.classList.contains('bodyOnAll')){
+        document.body.classList.add('bodyWidth');
+        document.body.classList.remove("bodyOnAll");
+      }
+      else if(document.body.classList.contains('bodyMobile')){
+        document.body.classList.add('bodyWidth');
+        document.body.classList.remove("bodyMobile");
+      }
+    }
+    if(window.innerWidth>1200){
+      if(document.body.classList.contains('bodyWidth')){
+        document.body.classList.add('bodyOnAll');
+        document.body.classList.remove("bodyWidth");
+      }
+      else if(document.body.classList.contains('bodyMobile')){
+        document.body.classList.add('bodyOnAll');
+        document.body.classList.remove("bodyMobile");
+      }
+    }
+    if(window.innerWidth<760){
+      if(document.body.classList.contains('bodyWidth')){
+        document.body.classList.add('bodyMobile');
+        document.body.classList.remove('bodyWidth');
+      }
+      else if(document.body.classList.contains('bodyOnAll')){
+        document.body.classList.add('bodyMobile');
+        document.body.classList.remove("bodyOnAll");
+      }
+    }
+    if(window.innerWidth<1200 && window.innerWidth>760) this.classToggled=true;
+    else this.classToggled=false;
   }
 }
